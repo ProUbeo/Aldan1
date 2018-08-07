b9b8715681e0cacec038e04645ccb817cb492046
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-08-07 13:46:09 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [],  xp: []}).write()
+
+var prefix = ("m!")
+
+bot.on('ready', function() {
+    bot.user.setUsername("MagicBot")
+    bot.user.setPresence({ game: { name: 'brûler des mages malpoli'}, status: 'online'}) //en ligne;
+    console.log("Connected")
+});
+
+bot.login(process.env.TOKEN);
+
+
+bot.on('message', message => {
+
+}
+)
